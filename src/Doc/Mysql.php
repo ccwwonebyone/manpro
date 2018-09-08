19871239d695301ee0205a86b076e161b70e6276
ccwwonebyone <ccwwonebyone@163.com>
2018-09-08 15:47:30 +0800

    方法重命名

---
+++ src/Doc/Mysql.php
@@ -0,0 +1,17 @@
+<?php
+namespace Manpro\Doc;
+
+class Mysql
+{
+    protected $config = [
+        'host'=>'',
+        'user'=>'',
+        'pwd' =>'',
+        'port'=>'',
+    ];
+
+    public function __construct($config)
+    {
+
+    }
+}
