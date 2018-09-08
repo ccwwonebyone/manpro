77141d94adc2223b1377ac2ad869dab7fcf8d75b
ccwwonebyone <ccwwonebyone@163.com>
2018-09-08 15:06:38 +0800

    新增日期,时间戳等

--- src/Manpro.php
+++ src/Manpro.php
@@ -27,4 +27,64 @@ class Manpro
             }
         }
     }
+
+    /**获取Unix毫秒时间戳
+     * @return float Unix毫秒时间戳
+     */
+    public function FunctionName()
+    {
+        list($msec, $sec) = explode(' ', microtime());
+        return (int)$sec.str_pad((int)($msec*1000),3,"0",STR_PAD_LEFT);
+    }
+
+    /**
+     * 解析最小代码
+     * ['1215','121516','121416','12141617']  返回 ['1215','121416']
+     * @param  array $areacodes 部门代码
+     * @return array            解析出最顶级的部门代码
+     */
+    public function parseCode($areacodes)
+    {
+        $minLength = strlen(reset($areacodes));
+        $searchArr = array(current($areacodes));
+        foreach ($areacodes as $value) {
+            if($minLength < strlen($value)) continue;
+            if($minLength > strlen($value)){
+                $minLength = strlen($value);
+                $searchArr = array();
+                $searchArr[] = $value;
+            }
+            if($minLength == strlen($value)) $searchArr[] = $value;
+        }
+        $searchArr = array_unique($searchArr);
+        $checkAreacode = array_diff($areacodes,$searchArr);
+        foreach ($checkAreacode as $key => $value) {
+            foreach ($searchArr as $val) {
+                if(strpos($value,$val) === 0) unset($checkAreacode[$key]);
+            }
+        }
+        if(!empty($checkAreacode)) $searchArr = array_merge($searchArr,$this->parseCode($checkAreacode));
+        return $searchArr;
+    }
+
+    /**
+     * 获取两个日期之间的所有间隔数据 包含自身
+     * @param  Date $smallDate 较小的日期
+     * @param  Date $bigDate   较大的日期
+     * @param  string $format  生成日期的格式
+     * @param  string $interval 间隔
+     * @return array
+     */
+    public function betweenDates($smallDate, $bigDate, $format = 'Y-m-d', $interval = '+1 days')
+    {
+        $time1 = strtotime($smallDate); // 自动为00:00:00 时分秒 两个时间之间的年和月份
+        $time2 = strtotime($bigDate);
+        $datearr = array();
+        $datearr[] = date($format,$time1);
+        while( ($time1 = strtotime($interval, $time1)) <= $time2){
+              $datearr[] = date($format,$time1); // 取得递增月;
+        }
+        $datearr[] = date($format,$time2);
+        return array_unique($datearr);
+    }
 }
