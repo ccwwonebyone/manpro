6a213f31f38537da571215916d4b7f39defc0e88
ccwwonebyone <ccwwonebyone@163.com>
2018-10-17 11:33:58 +0800

    数据库markdown文档生成

--- src/Doc/Mysql.php
+++ src/Doc/Mysql.php
@@ -6,31 +6,78 @@ class Mysql
     protected $config = [
         'driver' => 'mysql',
         'host' => '127.0.0.1',
-        // 'port' => '3306',
+        'port' => '3306',
         'database' => '',
         'username' => '',
         'password' => '',
         // 'unix_socket' => '',
-        // 'charset' => 'utf8',
-        // 'collation' => 'utf8_general_ci',
-        // 'prefix' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_general_ci',
+        'prefix' => '',
         // 'strict' => false,
-        // 'engine' => null,
+        'engine' => null,
     ];
 
     protected $pdo;
 
     public function __construct($config)
     {
-        $config = array_merge($this->config, $config);
-        $dsn    = $config['driver'].':dbname='.$config['database'].';host='.$config['host'];
-        $this->pdo = new \PDO($dsn, $config['username'], $config['password']);
+        $this->config = array_merge($this->config, $config);
+        $dsn    = $this->config['driver'].':dbname='.$this->config['database'].';host='.$this->config['host'];
+        $this->pdo = new \PDO($dsn, $this->config['username'], $this->config['password']);
     }
 
     public function tabs()
     {
         $sql  = "show table status";
         return $this->pdo->query($sql)->fetchAll();
+    }
+
+    public function cloumns($table)
+    {
+        $sql = 'show full columns from ' . $table;
+        return $this->pdo->query($sql)->fetchAll();
+    }
+
+    public function databases()
+    {
+        $sql = 'show databases';
+        return $this->pdo->query($sql)->fetchAll();
+    }
+
+    public function docTable($tab)
+    {
+        $fields = [
+            'Field'=>'字段',
+            'Comment'=>'注释',
+            'Type'=>'类型',
+            'Null'=>'能否为null',
+            'Default'=>'默认值',
+            'Collation'=>'字符集',
+        ];
+        $content  = "---\r\n## *{$tab['Name']}-{$tab['Comment']}*\r\n";
+        $content .= "```\r\n注释：{$tab['Comment']}\r\nengine：{$tab['Engine']}\r\n字符集：{$tab['Collation']}\r\n```\r\n";
+        $content .= implode(' | ', $fields)."\r\n";
+        $content .= implode(' | ', array_fill(0, count($fields), '---'))."\r\n";
+        $cloumns = $this->cloumns($tab['Name']);
+        foreach ($cloumns as $k => $v) {
+            foreach ($fields as $key => $value) {
+                $temp[$key] = $v[$key];
+            }
+            $content .= implode(' | ', $temp) . "\r\n";
+        }
+        return $content;
+    }
+
+    public function markdown($file_path = './')
+    {
+        $tabs = $this->tabs();
+        $content = "[TOC]\r\n";
+        $content .= '# '.$this->config['database']."\r\n";
+        foreach ($tabs as $tab) {
+            $content .= $this->docTable($tab);
+        }
+        file_put_contents($file_path. $this->config['database']. '.md', $content);
     }
 
 }
