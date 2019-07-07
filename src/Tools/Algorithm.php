ce4a7ac2b4916c5c4290052e349fccabb3b99346
ccwwonebyone <ccwwonebyone@163.com>
2019-07-07 18:06:43 +0800

    浮点型转16进制，浮点型->计算器储存类型->16进制

--- src/Tools/Algorithm.php
+++ src/Tools/Algorithm.php
@@ -43,5 +43,52 @@ class Algorithm extends Tool
         return $searchArr;
     }
 
+    /**
+     * 浮点型转16进制，浮点型->计算器储存类型->16进制
+     * 浮点型x86cpu 小端 二进制码
+     * 1位     符号位
+     * 2-9位   指数位
+     * 10-32位 尾数位
+     * @param  float $float 浮点数字
+     * @return  string
+     */
+    public function floattohex(float $float)
+    {
+        //符号位
+        $sign = $float > 0 ? '0' : '1';
+        list($integer, $decimal) = explode('.', (string)$float);
+        $integer = decbin($integer);
 
+        $index_decimal = $decimal = floatval('0.'.$decimal);
+        //指数位
+        if(abs($float) >= 1){
+            $index = strlen($integer) - 1;
+        }else{
+            for ($i=0; $i < 23; $i++) {
+                $index_decimal = $index_decimal*2;
+                if($index_decimal >= 1){
+                    $index = -$i-1;
+                    break;
+                }
+            }
+        }
+        $index = sprintf('%08d', decbin(127 + $index));
+
+        //尾数位
+        $mantissa = abs($float) >= 1 ? $integer : '';
+        while (strlen($mantissa) < 24) {
+            $decimal = $decimal*2;
+            if($decimal >= 1){
+                $decimal = $decimal - 1;
+                $mantissa .= '1';
+            }else{
+                $mantissa .= $mantissa ? '0' : '';
+            }
+        }
+        $mantissa = substr($mantissa, 1);
+
+        //计算器储存类型
+        $binary = $sign . $index . $mantissa;
+        return base_convert($binary, 2, 16);
+    }
 }
