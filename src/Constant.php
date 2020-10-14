22543b7d733ed33610829605a98e47af6875b577
ccwwonebyone <ccwwonebyone@163.com>
2020-10-14 15:06:24 +0800

    常量记录

---
+++ src/Constant.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Manpro;
+
+class Constant
+{
+    /**
+     * 是
+     */
+    const IS = true;    //是
+
+    /**
+     * 否
+     */
+    const NOT_IS = false;   //否
+
+    /**
+     * 一个
+     */
+    const ONE = 1;  //一个
+
+    /**
+     * 是
+     */
+    const NUMBER_TRUE = 1;
+
+    /**
+     * 否
+     */
+    const NUMBER_FALSE = 2;
+
+    /**
+     * 男
+     */
+    const MAN = 1;
+
+    /**
+     * 女
+     */
+    const WOMAN = 2;
+
+    /**
+     * 男孩
+     */
+    const BOY = 1;
+
+    /**
+     * 女孩
+     */
+    const GIRL = 2;
+}
